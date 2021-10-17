b0f13da86778c7e67ca27acee9819f9f77ba9ac4
yuttei1234 <yuchin1234@gmail.com>
2021-10-17 23:38:36 +0000

    [upload]退会画面/機能追加

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -1,2 +1,22 @@
 class Public::ContactsController < ApplicationController
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_public_contact_path, notice:"Send succuessfully!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:user).permit(:title, :message)
+  end
+
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,2 +1,39 @@
 class Public::UsersController < ApplicationController
+
+ before_action :authenticate_user!, except: :top
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to public_user_path(@user.id), notice:"Update profile!"
+    else
+      render "users/edit"
+    end
+  end
+
+  def unsubscribe
+    @user = current_user
+  end
+
+  def withdraw
+    @user = current_user
+    @user.update(validation: true)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :postal_code, :address, :telephone_number)
+  end
+
 end
