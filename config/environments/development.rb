944082290b8cba221d6fe79103cde223f7dbc24a
yuttei1234 <yuchin1234@gmail.com>
2021-10-19 11:52:09 +0000

    [upload]メール機能実装中

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,20 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #以下開発環境下でのメール送信機能実装
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: 'gmail.com',
+    port:587,
+    user_name: Rails.application.credentials.gmail[:user_name],
+    password: Rails.application.credentials.gmail[:password],
+    authentication:plain,
+    enable_starttls_auto: true
+  }
+
 end
