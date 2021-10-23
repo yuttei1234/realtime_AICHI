3602ca9b4c58233b7e78207accdf48340d9900a4
yuttei1234 <yuchin1234@gmail.com>
2021-10-23 14:25:33 +0000

    [upload]投稿機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!,except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   def after_sign_in_path_for(resource)
     if current_user
       public_post_images_path
@@ -10,9 +11,9 @@ class ApplicationController < ActionController::Base
       admins_path
     end
   end
-  
+
   protected
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :telephone_number ])
   end
