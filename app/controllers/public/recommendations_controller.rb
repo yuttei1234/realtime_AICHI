11c79917cb7f1d7a437430ef417b2dd4fc879797
yuttei1234 <yuchin1234@gmail.com>
2021-10-24 12:13:37 +0000

    [upload]地図機能実装

--- app/controllers/public/recommendations_controller.rb
+++ app/controllers/public/recommendations_controller.rb
@@ -1,10 +1,10 @@
 class Public::RecommendationsController < ApplicationController
-  
+
   def index
   end
-  
+
   def recommendation_params
     params.require(:recommendation).permit(:address, :latitude, :longitude)
   end
-  
+
 end
