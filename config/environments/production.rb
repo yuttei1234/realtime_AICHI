c6278b3cb2c82c77c3715933e642bcf50fc62562
yuttei1234 <yuchin1234@gmail.com>
2021-11-10 23:37:57 +0000

    [upload]デプロイ修正中

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   #以下本番環境下でのメール送信機能実装
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => 'http://35.168.39.184' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
