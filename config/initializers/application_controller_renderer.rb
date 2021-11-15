3740fea75716d8acd7a26fd20c338ae777bca4d8
yuttei1234 <yuchin1234@gmail.com>
2021-11-15 10:15:57 +0000

    [upload]投稿機能修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -4,3 +4,5 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+
+Refile.secret_key = '350143217bcb0f1ce0d77327181663a67ba3dc8e993220e5267295d9dc51d0b80b5cea3866b521090bb5feaa727e5ab0e6f8ad49ae3d1338f2e0378ed976ac63'
